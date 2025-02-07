bc116ae8ab1b624be03273ddc66cdf344e7c614a
AndreyChim <chimitcyrenov@mail.ru>
2025-02-07 16:51:55 +0800

    Fix component

--- app/components/view_components/button_component.rb
+++ app/components/view_components/button_component.rb
@@ -1,6 +1,24 @@
-# frozen_string_literal: true
 
-module ViewComponents
+module MyEngine
   class ButtonComponent < ViewComponent::Base
+    def initialize(label:, type: :primary)
+      @label = label
+      @type = type
+    end
+
+    private
+
+    attr_reader :label, :type
+
+    def button_classes
+      case type
+      when :primary
+        "bg-blue-500 text-white px-4 py-2 rounded"
+      when :secondary
+        "bg-gray-500 text-white px-4 py-2 rounded"
+      else
+        "bg-gray-300 text-black px-4 py-2 rounded"
+      end
+    end
   end
 end
